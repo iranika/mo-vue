da67dcf199fb3f4d294be48c9385a7cabef30c54
iranika <7370486+iranika@users.noreply.github.com>
2020-10-21 15:33:48 +0900

    Updates

---
+++ precache-manifest.fb3088fa09538b15998308a8e3cba93e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e347e1940face1c38add",
+    "url": "css/chunk-vendors.45c31457.css"
+  },
+  {
+    "revision": "e2a49f70bab99960bc28701e15ec9aa2",
+    "url": "index.html"
+  },
+  {
+    "revision": "f6b0faf2fcdc676756f9",
+    "url": "js/about.97f3ddf2.js"
+  },
+  {
+    "revision": "4bd24c3582a8d9fe7f26",
+    "url": "js/app.0b8841b4.js"
+  },
+  {
+    "revision": "e347e1940face1c38add",
+    "url": "js/chunk-vendors.c802d09d.js"
+  },
+  {
+    "revision": "44e2568a3620e04e73193499acfd05c9",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.2de42fb68fd82c07d8700adb37edad6d.js"
+  "precache-manifest.fb3088fa09538b15998308a8e3cba93e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "mo-vue"});
