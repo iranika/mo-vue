3e99b2bb2217973581d512ac9021f82e6db05fb4
iranika <7370486+iranika@users.noreply.github.com>
2020-10-23 01:16:15 +0900

    #1 : infinite scrollingの実装

--- src/components/ViewerContent.vue
+++ src/components/ViewerContent.vue
@@ -15,9 +15,46 @@
 <script>
 export default {
   data: () => ({
-    show: [0,1,2,3,4],
-    pages : window.pageData.slice(0,5)
+    show: [0],
+    pages : window.pageData,
+    bottom: false
   }),
+  methods: {
+    bottomVisible(){
+      const scrollY = window.scrollY;
+      const visible = document.documentElement.clientHeight;
+      const pageHeight = document.documentElement.scrollHeight;
+      const bottomOfPage = visible + scrollY >= pageHeight;
+      return bottomOfPage || pageHeight < visible
+    },
+    addContent(addnum = 1){
+      if (this.pages.length > this.show.length){
+        this.show.push(
+          Math.max.apply(null, this.show) + addnum
+        )
+        if (this.bottomVisible()){
+          //this.addContent()
+        }
+      }else{
+        console.log("viewerContent info: show.length is over pages.length")
+      }
+    }
+  },
+  watch:{
+    bottom(bottom){
+      if (bottom){
+        if (bottom){
+          this.addContent()
+        }
+      }
+    }
+  },
+  created(){
+    window.addEventListener("scroll", ()=>{
+      this.bottom = this.bottomVisible();
+    })
+    this.addContent()
+  }
 
 };
 </script>
