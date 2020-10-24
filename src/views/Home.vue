d53f23d1bc05b7af6f96af2363a8f27f849e590d
iranika <7370486+iranika@users.noreply.github.com>
2020-10-25 05:33:22 +0900

    Fix: 不要なパラメーターの削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <ViewerContent></ViewerContent>
-    <div >Todo: footerコンポーネントを実装する </div>
+    <div>Todo: Footerの実装</div>
   </div>
 </template>
 
@@ -12,8 +12,6 @@ import ViewerContent from "../components/ViewerContent.vue";
 export default {
   name: "Home",
   data: () => ({
-    show: [0,1,2,3,4],
-    pages : window.pageData.slice(0,5)
   }),
   components: {
     ViewerContent
