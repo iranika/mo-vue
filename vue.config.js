5f270da594d7e2e75ae1ea50c438de3c74439228
iranika <7370486+iranika@users.noreply.github.com>
2021-02-09 17:41:42 +0900

    Fix: 実在しないファイルの除外

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     appleMobileWebAppStatusBarStyle: 'default',
     workboxPluginMode: 'GenerateSW',
     workboxOptions:{
-    exclude: ["CNAME", /chunk-vendors.*.css.map/ , /about.*.css.map/ ]
+    exclude: ["CNAME", /chunk-vendors.*.css.map/ , /about.*.css.map/ , /app.*.css.map/]
     }
   }
 };
