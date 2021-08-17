71ffbe0cb38b6e4c81f0fc202fe9737ebf5b31e9
iranika <7370486+iranika@users.noreply.github.com>
2021-08-17 18:40:23 +0900

    add: google analytics

---
+++ src/views/DLsiteProducts.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    result:{{result}}
+  </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue } from "vue-property-decorator";
+import Footer from "../components/Footer.vue";
+import axios from "axios";
+
+@Component({
+  components: { Footer },
+})
+export default class DLsiteProducts extends Vue {
+  private result = "";
+  mounted(){
+    const url = "https://script.google.com/macros/s/AKfycbzfu6uBPQPDJwZlDt1Qpa_9GCzOT58t6tlF_vLoQI3IeISx_CaudS05RlaB7AUIOu9QOA/exec";
+    axios.get(url + "?ids=RJ3333,RJ4444,RJ5555").then(res=>{
+      console.log(res.data);
+      this.result = res.data;
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
